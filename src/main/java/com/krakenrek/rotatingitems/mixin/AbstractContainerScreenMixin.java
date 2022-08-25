1a395d5bc0a43efc1a3955d812d0683d561169d9
Krakenrek <raumkind@outlook.com>
2022-08-25 20:57:51 +0300

    some changes

--- src/main/java/com/krakenrek/rotatingitems/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/com/krakenrek/rotatingitems/mixin/AbstractContainerScreenMixin.java
@@ -4,6 +4,7 @@ import com.krakenrek.rotatingitems.Config;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,10 +16,12 @@ public class AbstractContainerScreenMixin extends Screen {
     protected AbstractContainerScreenMixin(Component p_96550_) {
         super(p_96550_);
     }
+
     @Inject(method = "renderSlotHighlight*", at = @At("HEAD"), cancellable = true)
     private static void disableHighlight(CallbackInfo ci) {
-        if (Config.CLIENT.disableHighlightInHovered.get() && (Config.CLIENT.rotationType.get().equals(Config.RotationType.HOVERED))) {
+        if (Config.CLIENT.disableHighlightInHovered.get()) {
             ci.cancel();
         }
     }
+
 }
